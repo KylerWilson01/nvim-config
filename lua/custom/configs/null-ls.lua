8cded53e16080c49151b4908d1c2228be0f6f6aa
Kyler Wilson <kyler@viticusgroup.org>
2024-02-08 13:40:29 -0800

    python

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -6,6 +6,8 @@ local opts = {
     null_ls.builtins.code_actions.eslint,
     null_ls.builtins.code_actions.refactoring,
     null_ls.builtins.diagnostics.stylelint,
+    null_ls.builtins.diagnostics.ruff,
+    null_ls.builtins.formatting.black,
     null_ls.builtins.formatting.jq,
     null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.stylua,
