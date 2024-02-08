c3c962571ca37fb55445c1ab2df175edc86c30d8
Kyler Wilson <kyler.wilson.1@proton.me>
2024-02-07 20:13:04 -0800

    took out nix from null-ls

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -3,13 +3,10 @@ local null_ls = require 'null-ls'
 
 local opts = {
   sources = {
-    null_ls.builtins.code_actions.statix,
     null_ls.builtins.code_actions.eslint,
     null_ls.builtins.code_actions.refactoring,
     null_ls.builtins.diagnostics.stylelint,
-    null_ls.builtins.diagnostics.statix,
     null_ls.builtins.formatting.jq,
-    null_ls.builtins.formatting.nixfmt,
     null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.csharpier,
