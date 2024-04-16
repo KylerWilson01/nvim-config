a0f9cac1b995e4fab3dfa3343a6644671b63d087
Kyler Wilson <kyler@viticusgroup.org>
2024-04-16 15:29:13 -0700

    use add not append

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -9,7 +9,7 @@ return {
     harpoon:setup {}
 
     vim.keymap.set('n', '<leader>ha', function()
-      harpoon:list():append()
+      harpoon:list():add()
     end, { silent = true, desc = '[H]arpoon mark [A]dd file' })
 
     vim.keymap.set('n', '<leader>hm', function()
